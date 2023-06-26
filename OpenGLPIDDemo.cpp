805c3e3dba3f30a58d6c86861f2212227973b08c
Adelino Penedo <agpene@hotmail.com>
2023-06-26 01:46:45 -0300

    Showing values on screen

--- OpenGLPIDDemo.cpp
+++ OpenGLPIDDemo.cpp
@@ -1,16 +1,19 @@
 #include <GL/glut.h>
+#include <GL/freeglut.h>
 #include <stdio.h>
 #include <corecrt_math.h>
 #include <corecrt_math_defines.h>
 #include <iostream>
+#include <sstream>
 #include <list>
-#include "OpenGLPIDDemo.h"
 #include <functional>
 
 using namespace std;
 
 #define XDMAX 1000
 #define YDMAX 660
+
+#define DEBUG_MODE false
 
 class Color {
 public:
@@ -25,7 +28,15 @@ public:
         this->blue = blue;
         this->alpha = alpha;
     }
+    Color(float red, float green, float blue) {
+        this->red = red;
+        this->green = green;
+        this->blue = blue;
+        this->alpha = 1;
+    }
 };
+
+const Color white = Color(1, 1, 1);
 
 class Vector2 {
 public:
@@ -73,8 +84,8 @@ public:
     list<Vector2> currentSimulatedValues;
 
     list<Vector2> SimulateTillDesired() {
+        if(DEBUG_MODE) cout << "Valor Inicial: "<< initialValue << " Valor desejado: "<< desiredValue << endl;
 
-        cout << "Valor Inicial: "<< initialValue << " Valor desejado: "<< desiredValue << endl;
         currentTick = 0;
         currentValue = initialValue;
 
@@ -115,8 +126,7 @@ public:
         cumulative_error += error;
         previous_error = error;
 
-
-        cout << "Valor PID"<< pid_value  << "Valor Atual:" << currentValue << endl;
+        if (DEBUG_MODE) cout << "Valor PID"<< pid_value  << "Valor Atual:" << currentValue << endl;
         return pid_value;
     }
 };
@@ -131,44 +141,6 @@ float normalizeFloat(float initialValue, float endValue, float current) {
     return  (current - initialValue) / (endValue - initialValue);
 }
 
-void RenderBackGround() {
-    glColor3f(0.3 , 0.3, 0.3);
-    glLineWidth(4);
-    glRectf(0, 0, XDMAX / 3, YDMAX);
-
-    glLineStipple(1, 10);
-    glColor3f(1, 0.5f, 0);
-    glLineWidth(3);
-
-    glEnable(GL_LINE_STIPPLE);
-    glBegin(GL_LINE_STRIP);
-        glVertex2f((XDMAX / 3) + 10, 10);
-        glVertex2f((XDMAX / 3) + 10, YDMAX / 2);
-        glVertex2f(XDMAX * 2 - XDMAX / 3, YDMAX / 2);
-        glVertex2f((XDMAX / 3) + 10, YDMAX / 2);
-        glVertex2f((XDMAX / 3) + 10, YDMAX - 10);
-    glEnd();
-    glDisable(GL_LINE_STIPPLE);
-}
-
-void RenderSimulatedValues(PIDController *pidController) {
-    glColor3f(0, 0.5f, 1);
-    glLineWidth(4);
-
-    Vector2 initialPoint = Vector2((XDMAX / 3) + 11, 0);
-    glBegin(GL_LINE_STRIP);
-    int valuesSize = pidController->currentSimulatedValues.size();
-    float initialValue = pidController->initialValue;
-    float desiredValue = pidController->desiredValue * 2;
-
-    for (Vector2 vec : pidController->currentSimulatedValues) {
-        glVertex2f(initialPoint.x + (normalizeFloat(0, valuesSize, vec.x) * XDMAX), normalizeFloat(initialValue, desiredValue, vec.y) * YDMAX);
-
-        cout << "Valor x: "<< initialPoint.x + (normalizeFloat(0, valuesSize, vec.x) * XDMAX) << " Valor Y: " << normalizeFloat(pidController->initialValue, pidController->desiredValue, vec.y) * YDMAX << endl;
-    }
-     
-    glEnd();
-}
 
 
 void ChangeSize(GLsizei w, GLsizei h) {
@@ -197,11 +169,101 @@ void SpecialKeyEvent(int tecla, int xmouse, int ymouse) {
     glutPostRedisplay();
 }
 
+void RenderString(float x, float y, void* font, const char* string, Color const& rgb)
+{
+    glColor4f(rgb.red, rgb.green, rgb.blue, rgb.alpha);
+    glRasterPos2f(x, y);
+    const unsigned char* str = reinterpret_cast<const unsigned char*>(string);
+    glutBitmapString(font, str);
+}
+
+void RenderBackGround() {
+    glColor3f(0.3, 0.3, 0.3);
+    glLineWidth(4);
+    glRectf(0, 0, XDMAX / 3, YDMAX);
+
+    glLineStipple(1, 10);
+    glColor3f(1, 0.5f, 0);
+    glLineWidth(3);
+
+    glEnable(GL_LINE_STIPPLE);
+    glBegin(GL_LINE_STRIP);
+    glVertex2f((XDMAX / 3) + 10, 10);
+    glVertex2f((XDMAX / 3) + 10, YDMAX / 2);
+    glVertex2f(XDMAX * 2 - XDMAX / 3, YDMAX / 2);
+    glVertex2f((XDMAX / 3) + 10, YDMAX / 2);
+    glVertex2f((XDMAX / 3) + 10, YDMAX - 10);
+    glEnd();
+    glDisable(GL_LINE_STIPPLE);
+}
+
+void RenderSimulatedValues(PIDController* pidController) {
+    glColor3f(0, 0.5f, 1);
+    glLineWidth(4);
+
+    Vector2 initialPoint = Vector2((XDMAX / 3) + 11, 0);
+    glBegin(GL_LINE_STRIP);
+    int valuesSize = pidController->currentSimulatedValues.size();
+    float initialValue = pidController->initialValue;
+    float desiredValue = pidController->desiredValue * 2;
+
+    for (Vector2 vec : pidController->currentSimulatedValues) {
+        glVertex2f(initialPoint.x + (normalizeFloat(0, valuesSize, vec.x) * XDMAX), normalizeFloat(initialValue, desiredValue, vec.y) * YDMAX);
+
+        if (DEBUG_MODE) cout << "Valor x: " << initialPoint.x + (normalizeFloat(0, valuesSize, vec.x) * XDMAX) << " Valor Y: " << normalizeFloat(pidController->initialValue, pidController->desiredValue, vec.y) * YDMAX << endl;
+    }
+
+    glEnd();
+
+}
+
+void RenderControlElement(Vector2 position, const char* titleString, const char* valueString) {
+    RenderString(position.x, position.y, GLUT_BITMAP_TIMES_ROMAN_24, titleString, white);
+    RenderString(position.x, position.y - 30, GLUT_BITMAP_TIMES_ROMAN_24, valueString, white);
+}
+
+void RenderControls(PIDController* pidController) {
+    Vector2 elementPosition = Vector2(30, YDMAX - 30);
+    ostringstream ss;
+
+    ss << "Q+  " << pidController->desiredValue << "  -W";
+    RenderControlElement(elementPosition, "Valor Desejado:", ss.str().c_str());
+    elementPosition.y -= 60;
+    ss.str("");
+    ss.clear();
+
+    ss << "A+  " << pidController->initialValue << "  -S";
+    RenderControlElement(elementPosition, "Valor Inicial:", ss.str().c_str());
+    elementPosition.y -= 60;
+    ss.str("");
+    ss.clear();
+
+    ss << "R+  " << pidController->Kp << "  -T";
+    RenderControlElement(elementPosition, "Fator P:", ss.str().c_str());
+    elementPosition.y -= 60;
+    ss.str("");
+    ss.clear();
+
+    ss << "F+  " << pidController->Ki << "  -G";
+    RenderControlElement(elementPosition, "Fator I:", ss.str().c_str());
+    elementPosition.y -= 60;
+    ss.str("");
+    ss.clear();
+
+    ss << "V+  " << pidController->Kd << "  -B";
+    RenderControlElement(elementPosition, "Fator D:", ss.str().c_str());
+    elementPosition.y -= 60;
+    ss.str("");
+    ss.clear();
+}
+
 void RenderScene()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
     RenderBackGround();
+
+    RenderControls(controllerPtr);
 
     RenderSimulatedValues(controllerPtr);
 
